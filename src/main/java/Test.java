1d90ff3a04f018934bb7d74e23add6376e0ce501
scott1541 <scott3933@gmail.com>
2017-08-24 14:09:34 +0100

    SimpleOutput

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Scott on 24/08/2017.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World");
+        System.out.println("THis is working!");
+    }
+}
